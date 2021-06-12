5863da47f9f8ee9c7a1d5b803935b292816dde8d
Yoshikatsu-Ka <yoshikatsu830@gmail.com>
2021-06-12 14:28:14 +0900

    コードの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,12 +8,12 @@ class CommentsController < ApplicationController
       @prototype = @comment.prototype
       @comments = @prototype.comments
       render "prototypes/show"
-    end
-    
+    end  
   end
 
  private
+
   def comment_params
-  params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
